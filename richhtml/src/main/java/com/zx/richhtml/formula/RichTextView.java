c67bfad4128d8a129cae09162fe9366a10f1d03e
Aige <aigestudio@hotmail.com>
2024-05-21 14:14:25 +0800

    Sync dependencies version as same as project.

--- richhtml/src/main/java/com/zx/richhtml/formula/RichTextView.java
+++ richhtml/src/main/java/com/zx/richhtml/formula/RichTextView.java
@@ -18,6 +18,7 @@ import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.zx.richhtml.R;
 import com.zx.richhtml.htmltext.HtmlImageLoader;
@@ -67,6 +68,63 @@ public class RichTextView extends AppCompatTextView {
         HtmlText.from(text)
                 .setImageLoader(htmlImageLoader != null ? htmlImageLoader : buildDefaultHtmlImageLoader())
                 .setOnTagClickListener(onTagClickListener != null ? onTagClickListener : buildDefaultOnTagClickListener())
+                .setImageLoader(new HtmlImageLoader() {
+                    @Override
+                    public void loadImage(String url, final Callback callback) {
+                        // Glide sample, you can also use other image loader
+                        Glide.with(mContext)
+                                .asBitmap()
+                                .load(url).into(new CustomTarget<Bitmap>() {
+                                    @Override
+                                    public void onResourceReady(
+                                            @NonNull final Bitmap bitmap,
+                                            @Nullable final Transition<? super Bitmap> transition
+                                    ) {
+                                        callback.onLoadComplete(bitmap);
+                                    }
+
+                                    @Override
+                                    public void onLoadCleared(@Nullable final Drawable drawable) {
+                                        callback.onLoadFailed();
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public Drawable getDefaultDrawable() {
+                        return ContextCompat.getDrawable(mContext, R.mipmap.image_placeholder_loading);
+                    }
+
+                    @Override
+                    public Drawable getErrorDrawable() {
+                        return ContextCompat.getDrawable(mContext, R.mipmap.image_placeholder_fail);
+                    }
+
+                    @Override
+                    public int getMaxWidth() {
+                        return getTextWidth();
+                    }
+
+                    @Override
+                    public boolean fitWidth() {
+                        return false;
+                    }
+                })
+                .setOnTagClickListener(new OnTagClickListener() {
+                    @Override
+                    public void onImageClick(Context context, List<String> imageUrlList, int position) {
+                        // image click
+                        Toast.makeText(context, imageUrlList.get(position), Toast.LENGTH_SHORT).show();
+                        Log.i("zxzx", "========" + imageUrlList.get(position));
+                    }
+
+                    @Override
+                    public void onLinkClick(Context context, String url) {
+                        // link click
+                        Toast.makeText(context, url, Toast.LENGTH_SHORT).show();
+                        Log.i("zxzx", "========" + url);
+                    }
+                })
                 .into(this);
     }
 
